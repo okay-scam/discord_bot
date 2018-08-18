7353aa7d6059d1b9e59ee1fb28d53e0a3c1e000d
Ubuntu <ubuntu@ip-172-31-10-57.ap-southeast-2.compute.internal>
2018-08-18 11:02:23 +0000

    Implemented time.py cog to track time spent in voice channels

--- cogs/time.py
+++ cogs/time.py
@@ -1,31 +1,58 @@
 import discord
 import asyncio
-import pandas
+import datetime
 import bot
 from pprint import pprint
-from discord.ext import commands
 
 class TimeCog():
     def __init__(self, bot):
         self.bot = bot
 
     async def on_voice_state_update(self, before, after):
-        table = bot.db['time']
+        time_table = bot.db['time']
+        sessions_table = bot.db['sessions']
         user = '{}#{}'.format(after.name, after.discriminator)
 
-        if (after.id is not '471255864238538753'
-            and after.voice_channel is not after.server.afk_channel):
+        if after.id == '471255864238538753':
+            return
+       
+        # User joins voice channel
+        if (before.voice_channel is None
+            and after.voice_channel is not None
+            or before.voice_channel is before.server.afk_channel):
 
-            # User leaves voice channel
-            if (before.voice_channel is not None
-                and after.voice_channel is None):
-                table.upsert(dict(user_id=after.id, user=user, join_timestamp=pandas.Timestamp.now()), ['join_timestamp'])
+            user_session_row = sessions_table.find_one(user_id=after.id)
+            join_timestamp = datetime.datetime.now()
+     
+            if user_session_row is None:
+                print('User does not have any prior sessions, creating entry')
+                sessions_table.insert(dict(user_id=after.id, name=user, session=None))
+                user_session_row = sessions_table.find_one(user_id=after.id)
 
-            # User joins voice channel
-            if (before.voice_channel is None
-                and after.voice_channel is not None):
-                table.upsert(dict(user_id=after.id, user=user, leave_timestamp=pandas.Timestamp.now()), ['user_id'])
-                pprint(list(table))
+            # Find and delete any sessions that went wrong (ie. bot crashed, user left and never got a leave timestamp)
+            for row in list(time_table.find(user_id=after.id)):
+                if row['join_timestamp'] is None and row['leave_timestamp'] is not None:
+                    time_table.delete(id=row['id'])
+
+            time_table.insert(dict(user_id=after.id, user=user, join_timestamp=join_timestamp, leave_timestamp=join_timestamp))
+            sessions_table.update(dict(user_id=after.id, session='active'), ['user_id'])
+            print('{} join_timestamp set to {}'.format(after.name, join_timestamp))
+
+
+        # User leaves voice
+        if (before.voice_channel is not None
+            and after.voice_channel is None
+            or after.voice_channel is after.server.afk_channel):
+
+            sessions_table.update(dict(user_id=after.id, session='inactive'), ['user_id'])
+            for row in list(time_table.find(user_id=after.id)):
+                if row['join_timestamp'] == row['leave_timestamp']:
+                    leave_timestamp = datetime.datetime.now()
+                    time_table.update(dict(id=row['id'], leave_timestamp=leave_timestamp), ['id'])
+                    delta = time_table.find_one(id=row['id'])
+                    session_time = delta['leave_timestamp'] - delta['join_timestamp']
+                    await self.bot.send_message(self.bot.get_channel('471262806533079041'), 
+                        '{} session time: {} minute(s), {} second(s)'.format(after.name, round(session_time.seconds/60), session_time.seconds))
 
 
 def setup(bot):
