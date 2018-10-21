c3c0eaa56f0746575afe94b32d09857306e3a249
Ubuntu <ubuntu@ip-172-31-10-57.ap-southeast-2.compute.internal>
2018-10-21 01:52:26 +0000

    Refactored time.py to record time every N seconds

--- cogs/new_time.py
+++ cogs/new_time.py
@@ -1,27 +1,67 @@
 import discord
-import datetime
 import asyncio
-
+import bot
+import operator
+import time
+import datetime
+import pendulum
+from cogs.utils import utils
+from collections import OrderedDict
+from tabulate import tabulate
+from discord.ext import commands
 
 class NewTimeCog():
     def __init__(self, bot):
         self.bot = bot
 
-    async def store_time_task(self):
-        await client.wait_until_ready()
-        time_table = bot.db['time']
-        sessions_table = bot.db['sessions']
+    @commands.command(pass_context=True)
+    async def newvoicetime(self, ctx):
+        time_table = bot.db['new_time']
+        highscores = dict()
+        highscores_for_msg = OrderedDict()
+        
+        for user in time_table:
+            highscores[user['user']] = user['time_in_seconds']
+        
+        highscores = sorted(highscores.items(),
+                            key=operator.itemgetter(1),
+                            reverse=True)
 
-        while not bot.client.is_closed():
-            members = []
-            try:
-                channels = bot.server.channels
-                for channel in channels:
-                    if channel.type == 'voice':
-                        members += channel.voice_members
-                for member in members:
-                    print('something like {}.total_time += 60s'.format(member))
-                await asyncio.sleep(60)
-            except Exception as e:
-                print(str(e))
-                await asyncio.sleep(60)
+        for user in highscores[:10]:
+            highscores_for_msg[user[0]] = utils.pretty_print_pendulum_duration(
+                pendulum.duration(seconds=user[1]))
+
+        highscores_msg = tabulate(
+            highscores_for_msg.items(),
+            tablefmt='plain',
+            showindex=range(1,11)
+        )
+           
+        await self.bot.say('```{}```'.format(highscores_msg))
+
+    @commands.command(pass_context=True)
+    async def newvoicetimeall(self, ctx):
+        time_table = bot.db['new_time']
+        highscores = dict()
+        highscores_for_msg = OrderedDict()
+        
+        for user in time_table:
+            highscores[user['user']] = user['time_in_seconds']
+        
+        highscores = sorted(highscores.items(),
+                            key=operator.itemgetter(1),
+                            reverse=True)
+
+        for user in highscores:
+            highscores_for_msg[user[0]] = utils.pretty_print_pendulum_duration(
+                pendulum.duration(seconds=user[1]))
+
+        highscores_msg = tabulate(
+            highscores_for_msg.items(),
+            tablefmt='plain',
+            showindex=range(1,len(highscores)+1))
+           
+        await self.bot.say('```{}```'.format(highscores_msg))
+
+def setup(bot):
+    bot.add_cog(NewTimeCog(bot))
