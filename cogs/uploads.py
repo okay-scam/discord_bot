77f7b59863aa2438014bcfea1d746c16a5cbafe1
Ubuntu <ubuntu@ip-172-31-10-57.ap-southeast-2.compute.internal>
2019-05-11 03:45:04 +0000

    Added message cleanup and checking

--- cogs/uploads.py
+++ cogs/uploads.py
@@ -3,29 +3,41 @@ import asyncio
 import bot
 import requests
 import boto3
-from boto.s3.connection import S3Connection
 from discord.ext import commands
 
 
-class Uploads():
+class Uploads:
     def __init__(self, bot):
         self.bot = bot
+        self.bot_id = "471255864238538753"
 
     async def on_message(self, message):
+        import pdb;pdb.set_trace()
+        if message.channel.id != "574866821061017600":
+            return
+
+        try:
+            file_name = message.attachments[0]["filename"] or None
+        except IndexError:
+            if message.author.id != self.bot_id:
+                await self.bot.delete_message(message)
+            return
         # upload-sounds text channel
-        if message.channel.id == '574866821061017600':
-            if message.attachments[0]['filename'].endswith('.mp3'):
-                conn = S3Connection(bot.config['AWS_ACCESS_KEY_ID'], bot.config['AWS_SECRET_ACCESS_KEY'])
-                bucket = conn.get_bucket(bot.config['AWS_STORAGE_BUCKET_NAME'])
+        if file_name.endswith(".mp3"):
+            request = requests.get(message.attachments[0]["url"], stream=True)
+            session = boto3.Session(
+                aws_access_key_id=bot.config["AWS_ACCESS_KEY_ID"],
+                aws_secret_access_key=bot.config["AWS_SECRET_ACCESS_KEY"],
+            )
+            s3 = session.resource("s3")
+            bucket = s3.Bucket(bot.config["AWS_STORAGE_BUCKET_NAME"])
+            bucket.upload_fileobj(request.raw, file_name)
 
-                url = message.attachments[0]['url']
-                re = requests.get(url, stream=True)
-
-                session = boto3.Session(aws_access_key_id=bot.config['AWS_ACCESS_KEY_ID'], aws_secret_access_key=bot.config['AWS_SECRET_ACCESS_KEY'])
-                s3 = session.resource('s3')
-
-                bucket = s3.Bucket(bot.config['AWS_STORAGE_BUCKET_NAME'])
-                bucket.upload_fileobj(re.raw, message.attachments[0]['filename'])
+            if message.author.id != self.bot_id:
+                await self.bot.delete_message(message)
+            await self.bot.send_message(
+                message.channel, "{} has been uploaded.".format(file_name)
+            )
 
 
 def setup(bot):
